2e05ecdbcb52632821e4df84205abda4926d3868
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2023-01-23 09:11:13 -0600

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,58 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as cp from "child_process";
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind,
+	SocketTransport,
+	Executable
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	const transport: SocketTransport = { kind: TransportKind.socket, port: 7979 };
+	// const options: ExecutableOptions = { detached: true, shell: true };
+	const unicon: Executable = { command: 'ulsp', transport: transport };
+	const serverOptions: ServerOptions = {
+		run: unicon,
+		debug: unicon
+	};
+
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'uniconLanguageServer',
+		'Unicon Language Server',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}

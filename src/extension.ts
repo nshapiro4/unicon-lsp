dd965e6ae0056a7f221f2901b579cdcd2ccee21e
mstow597 <markstowell15@gmail.com>
2023-01-26 15:02:14 -0500

    edits -- client now functional

--- src/extension.ts
+++ src/extension.ts
@@ -29,7 +29,7 @@ export function activate(context: ExtensionContext) {
 	// Options to control the language client
 	const clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'icn' }],
+		documentSelector: [{ scheme: 'file', language: 'unicon' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
